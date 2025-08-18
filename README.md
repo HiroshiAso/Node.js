eeb8f43c7c9fa556d54c3e2398ebf1b2a85bc41b
Hiroshi <153479708+HiroshiAso@users.noreply.github.com>
2025-08-18 02:18:51 +0000

    1koma

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Node.js
-Node.js
+## 1コマ目の内容 - Node.jsの基本
+- Node.jsとは何か
+- Node.jsのインストール方法
+- Node.jsの実行環境の構築
+### 手順
+- app.jsファイル作成
+```javascript
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
-var server = http.createServer(
-    (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World\n');
-    }
-)
-server.listen(3000);
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
