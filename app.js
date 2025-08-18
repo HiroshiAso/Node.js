e4accd6d71a4ca6c09f453ef1b0cdeda480b9f61
Hiroshi <153479708+HiroshiAso@users.noreply.github.com>
2025-08-18 02:15:49 +0000

    1koma

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+var server = http.createServer(
+    (req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World\n');
+    }
+)
+server.listen(3000);
