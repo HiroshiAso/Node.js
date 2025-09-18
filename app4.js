30259a10303c0006b92f87eea831ec650b7dcffe
Hiroshi <153479708+HiroshiAso@users.noreply.github.com>
2025-09-18 09:38:37 +0000

    HTTPサーバーを作成し、index.htmlファイルを返す機能を追加

--- app4.js
+++ app4.js
@@ -1,26 +1,23 @@
+// モジュールの読み込み
 const http = require('http');
 const fs = require('fs');
-const querystring = require('querystring');
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    fs.readFile('index2.html', (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
+
+// HTTPサーバーの作成
+const server = http.createServer((request, response) => {
+    // 非同期でindex.htmlファイルを読み込み
+    fs.readFile('./index.html', 'utf-8', (err, data) => {
+        // レスポンスヘッダーの設定
+        response.setHeader('Content-Type', 'text/html');
+        // 読み込んだHTMLファイルをクライアントに送信
+        response.write(data);
+        // レスポンスの終了
+        response.end();
     });
-  } else if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => { body += chunk; });
-    req.on('end', () => {
-      const data = querystring.parse(body);
-      console.log('名前:', data.name);
-      console.log('メールアドレス:', data.email);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('success');
-    });
-  }
 });
 
+// サーバーの待機
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
+
