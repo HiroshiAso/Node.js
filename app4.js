e35a668c439624568e85cdb175d9a122c6aa5487
Hiroshi <153479708+HiroshiAso@users.noreply.github.com>
2025-09-22 07:19:14 +0000

    Add minimatch and picocolors packages with necessary files and configurations
    - Added minimatch package (version 5.1.6) with its package.json.
    - Added picocolors package (version 1.1.1) including its README, LICENSE, and type definitions.
    - Updated package-lock.json to include dependencies for ejs, minimatch, and picocolors.
    - Created package.json for the project with ejs as a dependency.

--- app4.js
+++ app4.js
@@ -1,23 +1,20 @@
-// モジュールの読み込み
 const http = require('http');
 const fs = require('fs');
-
-
-// HTTPサーバーの作成
+ 
+ 
 const server = http.createServer((request, response) => {
-    // 非同期でindex.htmlファイルを読み込み
-    fs.readFile('./index.html', 'utf-8', (err, data) => {
-        // レスポンスヘッダーの設定
-        response.setHeader('Content-Type', 'text/html');
-        // 読み込んだHTMLファイルをクライアントに送信
-        response.write(data);
-        // レスポンスの終了
-        response.end();
-    });
+ 
+  fs.readFile('./index.html', 'utf-8', (err, data) => {
+   
+    response.setHeader('Content-Type', 'text/html');
+   
+    response.write(data)
+    response.end();
+  });
 });
-
-// サーバーの待機
+ 
+ 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
-
+ 
